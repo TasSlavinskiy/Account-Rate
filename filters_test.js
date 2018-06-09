5a86c5b21d4c7094db538de9277d47e2f62e5572
TasSlavinskiy <tas.tastan@outlook.com>
2018-06-09 19:40:16 +0300

    Create test tusk, and write first form block

---
+++ filters_test.js
@@ -0,0 +1,23 @@
+
+Feature('SignIn');
+var assert = require('assert');
+// let filters = new DataTable(['Country', 'Service/Segment', 'Product', 'Brand', 'Industry Served']); //
+// filters.add(['Albania', '', '', '', '']); // adding records to a table
+// filters.add(['Albania', '3D Printing', '', '', '']);
+// filters.add(['Albania', '3D Printing', 'Aluminum', '', '']);
+// filters.add(['Albania', '3D Printing', 'Aluminum', 'Accura', '']);
+// filters.add(['Albania', '3D Printing', 'Aluminum', 'Accura', 'Automotive']);
+//
+// var arr = ['Country', 'Service/Segment', 'Product', 'Brand', 'Industry Served'];
+
+Scenario('test something', async (I) => {
+
+I.amOnPage('/');
+//I.fillField('Электронный адрес или номер телефона', 'taras.slavinskiy@gmail.com');
+//I.fillField('Пароль', '48791ndIGo654412');
+//I.click('Вход');
+//var a = await I.grabTextFrom('._1vp5');
+//assert.equal(a, 'Тарас');
+
+//pause();
+});
